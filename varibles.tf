a9245aaa8a6987ad345bf5236c98c16afff4d95b
Thato Pebane <jpebane@gmail.com>
2022-11-06 11:44:27 +0200

    Create varibles.tf

---
+++ varibles.tf
@@ -0,0 +1,28 @@
+variable "environment" {
+    description = "Env"
+    default     = "dev"
+}
+
+variable "teamcode" {
+    description = "Env"
+    default     = "dev"
+}
+
+variable "name" {
+    description = "Application Name"
+    type        = string
+}
+
+locals {
+    description = "Aplication Name"
+    app_name = "${var.name}-${var.environment}"
+}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "lambda_name" {
+    description = "Name for lambda function"
+    default = "lambda"
+}
