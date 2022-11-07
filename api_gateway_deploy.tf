b783748742228831387ca33de1c60711eb2dfa4a
Thato Pebane <jpebane@gmail.com>
2022-11-07 21:10:41 +0200

    Update api_gateway_deploy.tf

--- api_gateway_deploy.tf
+++ api_gateway_deploy.tf
@@ -6,13 +6,6 @@ resource "aws_api_gateway_deployment" "api" {
    aws_api_gateway_integration.api,
  ]
 
- # Redeploy when there are new updates
- triggers = {
-   redeployment = sha1(join(",", tolist(
-     jsonencode(aws_api_gateway_integration.api),
-   )))
- }
-
  lifecycle {
    create_before_destroy = true
  }
