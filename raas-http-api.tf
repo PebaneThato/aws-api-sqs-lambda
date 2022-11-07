56c2d81ec9de42772d1dc64e0d256651a6429432
Thato Pebane <jpebane@gmail.com>
2022-11-07 20:53:14 +0200

    Update raas-http-api.tf

--- raas-http-api.tf
+++ raas-http-api.tf
@@ -1,5 +1,5 @@
 resource "aws_apigatewayv2_api" "raas-http-api-tf" {
   name          = "raas-http-api-tf"
-  description = "Http POST records to SQS queue"
+  description   = "Http POST records to SQS queue"
   protocol_type = "HTTP"
 }
