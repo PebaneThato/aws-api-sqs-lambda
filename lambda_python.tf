db37830781a27ec8bb738d1e3eab7091a4eaa4ca
Thato Pebane <jpebane@gmail.com>
2022-11-09 11:57:51 +0200

    Create lambda_python.tf

---
+++ lambda_python.tf
@@ -0,0 +1,22 @@
+data "archive_file" "lambda_with_dependencies" {
+  source_dir  = "lambda/python/"
+  output_path = "lambda/python/${local.app_name}-${var.lambda_name}.zip"
+  type        = "zip"
+}
+
+resource "aws_lambda_function" "lambda_sqs" {
+  function_name    = "${local.app_name}-${var.lambda_name}"
+  handler          = "handler.lambda_handler"
+  role             = aws_iam_role.lambda_exec_role.arn
+  runtime          = "python3.7"
+
+  filename         = data.archive_file.lambda_with_dependencies.output_path
+  source_code_hash = data.archive_file.lambda_with_dependencies.output_base64sha256
+
+  timeout          = 30
+  memory_size      = 128
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_role_policy
+  ]
+}
