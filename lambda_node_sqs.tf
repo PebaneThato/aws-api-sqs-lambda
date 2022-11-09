735a62b1e10360f77d35de4b5e16480ac754b7e8
Thato Pebane <jpebane@gmail.com>
2022-11-09 16:05:44 +0200

    update lambda name and roles

--- lambda_node_sqs.tf
+++ lambda_node_sqs.tf
@@ -4,11 +4,11 @@ data "archive_file" "node_lambda_archive" {
   type        = "zip"
 }
 
-resource "aws_lambda_function" "results_updates_lambda" {
+resource "aws_lambda_function" "lambda_node_sqs" {
   function_name    = "${local.app_name}-${var.lambda_name}"
   handler          = "handler.lambda_handler"
   role             = aws_iam_role.lambda_role.arn
-  runtime          = "nodejs12.x"
+  runtime          = "nodejs16.x"
 
   filename         = data.archive_file.node_lambda_archive.output_path
   source_code_hash = data.archive_file.node_lambda_archive.output_base64sha256
@@ -16,4 +16,20 @@ resource "aws_lambda_function" "results_updates_lambda" {
   timeout          = 30
   memory_size      = 128
     
+}
+
+resource "aws_lambda_permission" "allows_sqs_to_trigger_lambda" {
+  statement_id  = "AllowExecutionFromSQS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_node_sqs.function_name
+  principal     = "sqs.amazonaws.com"
+  source_arn    = aws_sqs_queue.raas-queue.arn
+}
+
+# Trigger lambda on message to SQS
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+  batch_size       = 1
+  event_source_arn =  aws_sqs_queue.raas-queue.arn
+  enabled          = true
+  function_name    =  aws_lambda_function.lambda_node_sqs.arn
 }
