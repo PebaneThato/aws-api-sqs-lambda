05c2b4116a4eaa2ce7806b4e9a373c042c45af5f
Thato Pebane <jpebane@gmail.com>
2022-11-10 14:52:15 +0200

    Add apigateway lambda invocation role

--- api_gateway_authorizer.tf
+++ api_gateway_authorizer.tf
@@ -2,5 +2,23 @@ resource "aws_api_gateway_authorizer" "raas-rest-api-authorizer" {
   name                   = "raas-rest-api-authorizer"
   rest_api_id            = aws_api_gateway_rest_api.raas-rest-api-tf.id
   authorizer_uri         = aws_lambda_function.lambda_python_authorizer.invoke_arn
-  authorizer_credentials = aws_iam_role.invocation_role.arn
+  authorizer_credentials = aws_iam_role.apiSQS.arn
+}
+
+resource "aws_iam_role_policy" "invocation_policy" {
+  name = "default"
+  role = aws_iam_role.apiSQS.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "lambda:InvokeFunction",
+      "Effect": "Allow",
+      "Resource": "${aws_lambda_function.lambda_python_authorizer.arn}"
+    }
+  ]
+}
+EOF
 }
