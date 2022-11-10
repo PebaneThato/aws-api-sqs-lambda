c9a1c1970f74fe99cdf2abbd1bd9b8ae87a4af6f
Thato Pebane <jpebane@gmail.com>
2022-11-10 15:51:29 +0200

    Enable api authorization

--- raas-rest-api.tf
+++ raas-rest-api.tf
@@ -20,7 +20,7 @@ resource "aws_api_gateway_method" "method_send-client-data" {
     rest_api_id   = aws_api_gateway_rest_api.raas-rest-api-tf.id
     resource_id   = aws_api_gateway_resource.send-client-data.id
     http_method   = "POST"
-    authorization = "NONE"
+    authorization = "aws_lambda_function.lambda_python_authorizer.invoke_arn"
 
     request_parameters = {
       "method.request.path.proxy"        = false
